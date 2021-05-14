e6e21a62ff6d29e2dbc8cf5e198dfc88cb508acc
Đàm Thị Hồng Nhung <DamThiHongNhung2kTN@gmail.com>
2021-05-14 23:08:27 +0700

    Statistics category

--- category_statistics.py
+++ category_statistics.py
@@ -2,7 +2,6 @@
 import re
 
 list = []
-list2 = []
 list3 = []
 
 # lấy dữ liệu từ file number_of_category.txt
@@ -12,27 +11,20 @@ count = 0
 
 f = open ("category_in_line.txt", "w", encoding="utf8")
 for line in open(link, "r", encoding='utf8'):
-       list.extend(line.split(", "))
-
-for i in list:
-    Re = re.findall(r'(0|[1-9][0-9]*)[^(\s)]*$',i)
-
-    list2.extend(Re)
-
-# print(list2)
+       list.extend(re.findall(r':\s(0|[1-9][0-9]*)[,\}]',line))
 
 # convert list string to list int
-for i in range(0, len(list2)):
-    list2[i] = int(list2[i])
+for i in range(0, len(list)):
+    list[i] = int(list[i])
 
-f.write(str(list2) + "\n")
+f.write(str(list) + "\n")
 
 print("- Enter a any number: ", end='')
 count = 0
 k = input()
 k = int(k)
 
-for i in list2:
+for i in list:
     if i > k:
         count = count + 1
 
--- count_category.py
+++ count_category.py
@@ -1,5 +1,6 @@
 import collections
 import re
+category = []
 
 def data_analysis_input(link):
     count = 0
@@ -16,7 +17,7 @@ def data_analysis_input(link):
 
 
 # # ghi kết quả đếm được vào file number_of_category.txt
-def num_of_category(data,text):
+def num_of_category(data):
 
     # mở một file ghi lại số lần xuất hiện của từng category
     f = open ("number_of_category.txt", "w", encoding="utf8")
@@ -25,13 +26,34 @@ def num_of_category(data,text):
 
     f.write(str(c))#ghi dữ liệu
 
-    print("- The number of that category is: ", c[text])
+    return c
 
+def category_statistic(c,k):
+    count = 0
+    for i in c.items():#Lấy từng category và số lần xuất hiện của nó
+        if int(i[1]) > k:# i[1] là số lần xuất hiện
+            count += 1
+            category.append(i[0]) # i[0] là category
+    return count
 
 if __name__ == "__main__":
+
     link = "category_round_04.txt" # dữ liệu cuối cùng từ file category_filter.py
     data = data_analysis_input(link)
+    c = num_of_category(data)
+
+    #input:category
+    #output:Số lần xuất hiện của category đó
     print("- Enter any category: ", end='')
-    num_of_category(data,text = input())
-    
+    text = input() # Nhập category có cấu trúc : '<category>'
+    print("- The number of that category is: ", c[text],"\n")
+
+
+    print("- Enter a any number: ", end='')
+    k =input()
+    print("-> The number of categories greater than %2d is: "%(k) + category_statistic(c,int(k)),"\n")
+
+    #Các category có ít nhất k lần xuất hiện là:
+    print("Các category có ít nhất k lần xuất hiện là: ",category)
+
     
